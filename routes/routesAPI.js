aeaf911318283ae3fc843c0dd3ab1092609ab9f3
valentinPavon <vpavonlopez02@gmail.com>
2023-03-30 10:00:50 +0200

    end-points realizados

--- routes/routesAPI.js
+++ routes/routesAPI.js
@@ -1,3 +1,32 @@
 const express = require('express')
 
-const 
+const router = express.Router()
+
+const pool = require('../database')
+
+
+router.get('/',(req,res)=>{
+    pool.query('select * from dataUser',(err,response)=>{
+        res.json(response)
+    })
+})
+
+router.get('/:id',(req,res)=>{
+    pool.query('select * from dataUser where id ='+req.params.id,(err,response)=>{
+        res.json(response)
+    })
+})
+
+router.post('/',(req,res)=>{
+    pool.query("insert into dataUser(id,nombre,descripcion) values ('"+req.body.id+"','"+req.body.nombre+"','"+req.body.descripcion+"')", (err,response)=>{
+        res.json({status:'data saved'})
+    })
+})
+
+router.delete('/:id',(req,res)=>{
+    pool.query('delete from dataUser where id='+req.params.id,(err,response)=>{
+        res.json({status:'task deleted'})
+    })
+})
+
+module.exports = router
