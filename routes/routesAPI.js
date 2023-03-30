66c493fb29bb3603eb232b6a54de7d92ef360c19
valentinPavon <vpavonlopez02@gmail.com>
2023-03-30 10:59:25 +0200

    routes añadidas

--- routes/routesAPI.js
+++ routes/routesAPI.js
@@ -7,6 +7,7 @@ const pool = require('../database')
 
 router.get('/',(req,res)=>{
     pool.query('select * from dataUser',(err,response)=>{
+        console.log(response.length);
         res.json(response)
     })
 })
@@ -20,6 +21,17 @@ router.get('/:id',(req,res)=>{
 router.post('/',(req,res)=>{
     pool.query("insert into dataUser(id,nombre,descripcion) values ('"+req.body.id+"','"+req.body.nombre+"','"+req.body.descripcion+"')", (err,response)=>{
         res.json({status:'data saved'})
+    })
+})
+
+
+router.put('/:id',(req,res)=>{
+    pool.query("update dataUser set nombre='"+req.body.nombre+"', descripcion='"+req.body.descripcion+"' where id='"+req.params.id+"'",(err,response)=>{
+        if(response){
+            res.json({status:'task updated'})
+        }else if(err){
+            res.json({err:err})
+        }
     })
 })
 
